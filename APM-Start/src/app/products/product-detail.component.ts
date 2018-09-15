2c3f928e29648f72e7cfa0157e4fe2614a433b48
msobala <mateusz.sobala@capgemini.com>
2018-09-15 16:30:01 +0200

    add back button with router in code; add route guard with angular cli

--- APM-Start/src/app/products/product-detail.component.ts
+++ APM-Start/src/app/products/product-detail.component.ts
@@ -1,19 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
 import { IProduct } from './product';
 
 @Component({
-  selector: 'pm-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
 
-  pageTitle: 'Product Detail';
+  pageTitle = 'Product Detail';
   product: IProduct;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) {
+    console.log(this.route.snapshot.paramMap.get('id')); // snapshot to get only initial value
+    this.route.paramMap // observable for changing values
+      .subscribe(params => {
+        const id = +params.get('id');
+        console.log(`Observable id: ${id}`);
+      });
+  }
 
   ngOnInit() {
+    const id = +this.route.snapshot.paramMap.get('id'); // + for converting string to numeric value
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      'productId': id,
+      'productName': 'product name'
+    };
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
   }
 
 }
